fd0b4a547f0f0b866a4378be1f61cf97a9268e50
Andrey Izman <andreyizman9@gmail.com>
2021-11-23 07:21:15 +0100

    updated the codebase

--- src/LoadingScreen.tsx
+++ src/LoadingScreen.tsx
@@ -4,8 +4,6 @@ import { translate } from './i18n';
 
 import './LoadingScreen.css';
 
-const WaldurLogo = require('@waldur/auth/WaldurLogo.svg');
-
 export const LoadingScreen: FunctionComponent<{
   loading: boolean;
   error?: Error;
@@ -15,10 +13,8 @@ export const LoadingScreen: FunctionComponent<{
       {loading ? (
         <div className="loading-screen-container">
           <div className="loading-screen">
-            <img src={WaldurLogo} alt="Logo should be here" />
-            <h1 className="loading-title">
-              {translate('Loading assets, please wait.')}
-            </h1>
+            <img src="/images/login_logo.png" alt="Logo should be here" />
+            <h1 className="loading-title">{translate('Loading assets')}</h1>
           </div>
         </div>
       ) : null}
